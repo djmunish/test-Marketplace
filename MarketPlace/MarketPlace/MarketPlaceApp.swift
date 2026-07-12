473dc34b4d00b8c743c81e36e5d5ea9703db9ec1
Munish <munishsehdev@gmail.com>
2026-07-12 17:42:28 -0400

    Initial project setup

---
+++ MarketPlace/MarketPlace/MarketPlaceApp.swift
@@ -0,0 +1,21 @@
+//
+//  MarketPlaceApp.swift
+//  MarketPlace
+//
+//  Created by Munish Sehdev on 2026-07-12.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct MarketPlaceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
